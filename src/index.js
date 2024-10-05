6714d4b59cdb84f066ee870931527400bbaaf2ee
wangzhiwei <4567657@qq.com>
2024-10-05 15:34:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ponypressx.com": dockerHub,
+  "quay.ponypressx.com": "https://quay.io",
+  "gcr.ponypressx.com": "https://gcr.io",
+  "k8s-gcr.ponypressx.com": "https://k8s.gcr.io",
+  "k8s.ponypressx.com": "https://registry.k8s.io",
+  "ghcr.ponypressx.com": "https://ghcr.io",
+  "cloudsmith.ponypressx.com": "https://docker.cloudsmith.io",
+  "ecr.ponypressx.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ponypressx.com": dockerHub,
 };
 
 function routeByHosts(host) {
